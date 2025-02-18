c86c37d7b091c7101da34651a4728ae4f1bec2ff
fishfishfish359 <zhusiyu2223420124@stu.xjtu.edu.cn>
2025-02-18 14:56:18 +0800

    001
    相反数的寻找

---
+++ 001 6.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int N;
+
+int main()
+{
+	cin >> N;
+	vector<int> numbers(N);
+    for (int i = 0; i < N; ++i) 
+        cin >> numbers[i];
+ 	
+	unordered_set<int> numSet;
+    int Count = 0;
+ 
+    for (int num : numbers) {
+        if (numSet.find(-num) != numSet.end()) 
+            ++Count;
+        numSet.insert(num);
+    }
+ 
+    cout << Count << endl;
+	return 0;
+ } 
+
